ad5e831324bb50d1aaeb41aa96523eb4411adf28
Nazia Habib <habib.nazia@gmail.com>
2026-06-08 13:19:41 -0500

    Create transformer.py

---
+++ src/transformer.py
@@ -0,0 +1,35 @@
+from lark import Transformer
+
+from ast_nodes import (
+    Signal,
+    Constant,
+    Diff,
+    Cumsum,
+    Eq,
+    And,
+    Or
+)
+
+
+class ASTBuilder(Transformer):
+
+    def NAME(self, items):
+        return Signal(str(items))
+
+    def NUMBER(self, items):
+        return Constant(float(items))
+
+    def diff(self, items):
+        return Diff(items[0])
+
+    def cumsum(self, items):
+        return Cumsum(items[0])
+
+    def eq(self, items):
+        return Eq(items[0], items[1])
+
+    def and_expr(self, items):
+        return And(items[0], items[1])
+
+    def or_expr(self, items):
+        return Or(items[0], items[1])
